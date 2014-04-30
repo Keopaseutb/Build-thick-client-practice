d3a8d3b2854811e0375d6711d296636f7320392f
Apprentice <apprentice@dbc30.local>
2014-04-30 16:53:44 -0700

    We are showing quiz name upon page load.   *We are adding listeners to the quiz g etter function.   *added quiz template to view   *added quizshower function
    TODO: Show Questions

--- source/app/assets/javascripts/something.js
+++ source/app/assets/javascripts/something.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
-
- quiz = new QuizListGetter()
- question = new QuizQuestionGetter(1)
-questionShower = new QuestionShower()
+  ApplicationController()
 });
 
+/*******************************************/
+// CONTROLLER
+/********************************************/
 
+ApplicationController = function(){
+  var quiz = new QuizListGetter()
+  var quizShower = new QuizShower()
+  quiz.fetch()
+  $(quiz).on('quizUpdated', function (e,data) {
+    quizShower.draw(data)
+  })
+}
 
 QuizListGetter =function(geturl) {
   this.quizUrl =  geturl || "/quizzes.json"
@@ -13,18 +21,18 @@ QuizListGetter =function(geturl) {
 
 QuizListGetter.prototype = {
   fetch: function () {
+     var self = this
   $.ajax({
     url: this.quizUrl,  //*When you register this event listener remember to bind
     action: "GET",
     data: {session_key : "ajerjijaf"}
   }).done(function(returnData){
-    console.log(returnData)
+    $(self).trigger('quizUpdated', returnData )
   }).fail(function(){
     console.log("Error")
   })
   }
 }
-
 
 QuizQuestionGetter =function(quiz_number) {
   this.questionUrl =  '/quizzes/' + quiz_number + '/questions/next.json' || '/quizzes/1/questions/next.json'
@@ -37,14 +45,16 @@ QuizQuestionGetter.prototype = {
     action: "GET",
     data: {session_key : "ajerjijaf"}
   }).done(function(returnData){
-
+    debugger
       questionShower.draw(returnData.question)
   }).fail(function(){
     console.log("Error")
   })
   }
 }
-
+/*******************************************/
+//  VIEW
+/********************************************/
 
 QuestionShower = function() {
   this.source = $('#entry-template').html();
@@ -59,4 +69,15 @@ QuestionShower.prototype = {
   }
 }
 
-// GET /quizzes/1/questions/next.json
+QuizShower = function() {
+  this.source = $('#quiz-template').html();
+  this.template = Handlebars.compile(this.source)
+}
+
+QuizShower.prototype = {
+  draw: function (data) {
+    $('.container').append(this.template(data.quizzes[0]))
+  }
+}
+
+
