3a93213acf665c410b738b3888b73914a32c66cf
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2014-04-30 16:52:04 -0700

    returns success

--- source/app/assets/javascripts/quiz.js
+++ source/app/assets/javascripts/quiz.js
@@ -13,3 +13,18 @@ function getQuizzes(){
     console.log("Error!", serverdata)
   })
 }
+
+
+
+// Returns:
+
+// Success!
+// Object {quizzes: Array[1]}
+// quizzes: Array[1]
+// 0: Object
+// name: "Dev Bootcamp Questions"
+// quiz_id: 1
+// __proto__: Object
+// length: 1
+// __proto__: Array[0]
+// __proto__: Object
