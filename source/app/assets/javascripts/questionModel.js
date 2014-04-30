bc0ce3fab0f5e8c44f412e982a2a0a2c71c41bdd
Nathan Sass & Nina Hwang <pair+nathansass.nihwang@devbootcamp.com>
2014-04-30 16:56:48 -0700

    able to create a key for a user, able to send a session, TODO get questions

--- source/app/assets/javascripts/questionModel.js
+++ source/app/assets/javascripts/questionModel.js
@@ -4,14 +4,28 @@ function Client(method, action){
 }
 
 Client.prototype = {
-  request: function(){
+  request: function(sessionKey){
     var response = $.ajax({
       type: this.method,
-      url: this.action
+      url: this.action,
+      data: { session_key: sessionKey }//data.serialize()
     })
     return response
   }
 }
+
+function User(){}
+User.prototype = {
+  generateKey: function(){
+    var key = ''
+    for(var i = 0; i < 10; i++){
+      key += Math.floor((Math.random() * 10) + 1)
+    }
+    return key
+  }
+}
+
+
 
 
 
--- source/app/controllers/quizzes_controller.rb
+++ source/app/controllers/quizzes_controller.rb
@@ -3,6 +3,9 @@ class QuizzesController < ApplicationController
 
   # GET /quizzes.json
   def index
+    p "* "*30
+    p params
+    p "* "*30
     quizzes = Quiz.all
     render json: { quizzes: quizzes }.to_json
   end
