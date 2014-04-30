f1e0aeae92c8e1d55126495bbf1cce2547e87ebf
Alexandra & Kelley Puckett <pair+Answart.kpnetdev@devbootcamp.com>
2014-04-30 16:26:54 -0700

    Refactor show quiz to MVC setup

--- source/app/assets/javascripts/initializer.js
+++ source/app/assets/javascripts/initializer.js
@@ -1,4 +1,41 @@
+var handleJSON_quiz = function(data) {
+  console.log(data)
+  console.log("within handleJSON_quiz")
+  var source = $('#quiz-template').html();
+  var template = Handlebars.compile(source);
+  our_data = {
+    QuizId : data['quizzes'][0].quiz_id,
+    QuizName: data['quizzes'][0].name
+  }
+  $('.container').append(template(our_data));
+}
+
+
+  QuizListFetcher = function(opts) {
+    if (!opts) opts = {};
+    this.queryURL = opts.queryURL || '/quizzes.json'
+  }
+
+  QuizListFetcher.prototype = {
+    fetch: function() {
+      $.get(
+        this.queryURL,
+        { session_key: 'a124f87dec55da23' },
+        handleJSON_quiz )
+    }
+  };
+
+
+
+
 $(document).ready(function() {
+
+  console.log('it starts!');
+
+  new QuizListFetcher().fetch();
+});
+
+
 
   // console.log("ready? GO!")
   // var init = $.ajax({
@@ -13,29 +50,3 @@ $(document).ready(function() {
   // });
 
   // var putithere = $('.container')
-
-  console.log('it starts!');
-
-  QuizListFetcher = function(opts) {
-    if (!opts) opts = {};
-    this.queryURL = opts.queryURL || '/quizzes.json'
-    // debugger
-    console.log("inside quiz fetcher!")
-  }
-
-  QuizListFetcher.prototype = {
-    fetch: function() {
-      $.get(this.queryURL, { session_key: 'a124f87dec55da23' }, function(data) {
-        var source = $('#quiz-template').html();
-        var template = Handlebars.compile(source);
-        our_data = {
-          QuizId : data['quizzes'][0].quiz_id,
-          QuizName: data['quizzes'][0].name
-        }
-        $('.container').append(template(our_data));
-      })
-    }
-  };
-
-new QuizListFetcher().fetch();
-});
