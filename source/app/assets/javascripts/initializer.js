3ef258dde0af68e5c11e51ef4b34c54c7ebd111f
Alexandra & Kelley Puckett <pair+Answart.kpnetdev@devbootcamp.com>
2014-04-30 22:11:01 -0700

    adds event delegation listener, fires the ajax and gets an appropriate response from the controller - entire point of the exercise still todo

--- source/app/assets/javascripts/initializer.js
+++ source/app/assets/javascripts/initializer.js
@@ -1,19 +1,5 @@
 
 
-
-// var handleJSON_quiz = function(data) {
-//   console.log(data)
-//   console.log("within handleJSON_quiz")
-//   var source = $('#quiz-template').html();
-//   var template = Handlebars.compile(source);
-//   our_data = {
-//     QuizId : data['quizzes'][0].quiz_id,
-//     QuizName: data['quizzes'][0].name
-//   }
-//   $('.container').append(template(our_data));
-// }
-
-
 // =============================
 // VIEW/TEMPLATE
 // =============================
@@ -33,27 +19,18 @@ handleJSON.prototype = {
     $('.container').append(template(our_data));
   },
   show_question: function(data) {
-    console.log(this)
     var source = $('#question-template').html();
     var template = Handlebars.compile(source);
     our_question = {
       questionText : data.question.question,
+      // questionId   : data.question.question_id,
       choicesArray : data.question.choices
     }
     $('.a_question').append(template(our_question));
+  },
+  handle_answer: function(data) {
+    console.log("yay, heard back", data)
   }
-
-  // show_choices: function(data) {
-  //   console.log(data)
-  //   var source = $('#choices-template').html();
-  //   var template = Handlebars.compile(source);
-  //   console.log(data.question.choices[0].choice)
-  //   console.log(data.question.choices)
-  //   our_choices = { choices: [
-  //     {choicetext: data.question.choices}
-  //   ] }
-  //   $('.choices').append(template(our_choices));
-  // }
 }
 
 
@@ -92,16 +69,44 @@ questionFetcher.prototype = {
 }
 
 
+// Answers -------
 
+submitAnswer = function(choice) {
+  question_id = choice.getAttribute("class");
+  answer_id = choice.getAttribute("id");
+  ourData = { choice_id: answer_id, session_key: 'a124f87dec55da23' }
+  queryUrl = "/questions/" + question_id + "/answers.json"
+  $.post(
+    queryUrl,
+    ourData,
+    handleJSON.prototype.handle_answer
+)}
 
+answerListener = function(element){
+  this.element = element
+};
+
+answerListener.prototype = {
+  listen: function() {
+    this.element.on('click', 'button', function (e) {
+      submitAnswer(e.target)
+    })
+  }
+}
+
+      // var clickedButton = e.target;
+      // console.log("We're going to send a question_id of " + clickedButton.getAttribute("class") + "and a choice_id of " + clickedButton.getAttribute("id"));
 
 // =============================
 // START!
 // =============================
 
   $(document).ready(function() {
+    var element = $('.a_question');
+
     new quizFetcher().fetch_quiz();
     new questionFetcher().fetch_question();
+    new answerListener(element).listen();
   });
 
 
