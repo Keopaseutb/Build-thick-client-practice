ee717aff5377d4f21e4abdd6ec766128c33f424c
David Wilbanks <dwilbank@gmail.com>
2014-04-30 18:37:08 -0700

    first click handler

--- source/app/assets/javascripts/stuff.js
+++ source/app/assets/javascripts/stuff.js
@@ -1,18 +1,29 @@
-// alert('this works')
+//alert('this works')
+
+var Navigator = {
+  init: function(){
+    console.log("lala")
+  }
+}
+
 $(document).ready(function(){
 		$.get( '/quizzes.json',function(data) {
 		 console.log(data['quizzes'][0])
           
           // stored the html quiz template in a variable
-          var template = Handlebars.compile( $('#quizz').html());
+          var template = Handlebars.compile( $('#quiz_template').html());
            
           // adds the JSON data to the template
           var templateWithData = template(data['quizzes'][0])
           console.log(templateWithData)
         
           // appends the template
-           $(document.body).append( templateWithData)
+           $('.quizzes').append(templateWithData)
 
-		}  )
+      Navigator.init()
+    } )
+    
+    $('.quizzes').on('click', 'a', function() {console.log("clicked")})
+
 	});
 
